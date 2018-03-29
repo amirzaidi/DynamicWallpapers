702362d27342e35540a8471b3cfe0f45086561c8
Amir Zaidi <azaidi@live.nl>
2018-03-29 12:33:59 +0200

    Add settings gear for permission granting (WIP)

---
+++ app/src/main/java/amirz/dynamicwallpapers/Settings.java
@@ -0,0 +1,26 @@
+package amirz.dynamicwallpapers;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class Settings extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            // Display the fragment as the main content.
+            getFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new SettingsFragment())
+                    .commit();
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+    }
+}
