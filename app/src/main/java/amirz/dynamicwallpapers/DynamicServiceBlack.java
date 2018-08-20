b308845543b49236ff31eeabd464e5ddb25ecb6f
Amir Zaidi <azaidi@live.nl>
2018-08-20 20:55:15 +0200

    Update Dynamica icon and preview

--- app/src/main/java/amirz/dynamicwallpapers/DynamicServiceBlack.java
+++
@@ -1,7 +0,0 @@
-package amirz.dynamicwallpapers;
-
-/**
- * Duplicate DynamicService functionality, but trigger the black theme in Launcher3.
- */
-public class DynamicServiceBlack extends DynamicService {
-}
--- app/src/main/java/amirz/dynamicwallpapers/DynamicServiceWhite.java
+++
@@ -1,7 +0,0 @@
-package amirz.dynamicwallpapers;
-
-/**
- * Duplicate DynamicService functionality, but trigger the white theme in Launcher3.
- */
-public class DynamicServiceWhite extends DynamicService {
-}
